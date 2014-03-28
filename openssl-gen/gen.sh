debe4ac5c941c9dc60e17724f8b3cd9e4cefd625
Hans-Christoph Steiner <hans@eds.org>
2014-03-27 22:29:08 -0400

    first time getting the whole generation to HSM process working

--- openssl-gen/gen.sh
+++ openssl-gen/gen.sh
@@ -2,13 +2,35 @@
 #
 # http://android-dls.com/wiki/index.php?title=Generating_Keys
 
-openssl genrsa -out key.pem 2048
-openssl req -new -key key.pem -out request.pem
-openssl x509 -req -days 9999 -in request.pem -signkey key.pem -out certificate.pem
+set -e
+set -x
+
+## DO THIS FIRST!
+#touch passin.txt
+#chmod 0600 passin.txt
+#editor passin.txt
+cp passin.txt passout.txt
+
+#openssl genrsa -out secretkey.pem -passout file:passout.txt -aes128 -rand /dev/random 2048
+openssl genrsa -out secretkey.pem -passout file:passout.txt -aes128 2048
+openssl rsa -in secretkey.pem -pubout -passin file:passin.txt -out publickey.pem
+openssl req -passin file:passin.txt -new -key secretkey.pem -out request.pem \
+    -subj "/C=US/ST=New York/O=Guardian Project Test/CN=test.guardianproject.info/emailAddress=test@guardianproject.info"
+openssl x509 -passin file:passin.txt -req -days 9999 \
+    -in request.pem -signkey secretkey.pem -out certificate.pem
 
 # http://rostislav-matl.blogspot.nl/2012/04/using-smart-card-as-keystore-in-java.html
-openssl pkcs12 -export -out certificate.p12 -in certificate.pem -inkey key.pem
+openssl pkcs12 -export -out certificate.p12 -in certificate.pem -inkey secretkey.pem \
+    -passin file:passin.txt -passout file:passout.txt
 
-keytool -importkeystore -srckeystore openssl-gen/certificate.p12 -srcstoretype PKCS12 -destkeystore certificate.jkr -deststoretype JKS
+keytool -importkeystore \
+    -srckeystore certificate.p12 -srcstoretype PKCS12 -srcstorepass:file passin.txt \
+    -destkeystore certificate.jkr -deststoretype JKS -deststorepass:file passin.txt
 
-#openssl pkcs8 -topk8 -outform DER -in key.pem -inform PEM -out key.pk8 -nocrypt
+keytool -v \
+    -providerClass sun.security.pkcs11.SunPKCS11 \
+    -providerArg /home/hans/Desktop/smartcards/opensc-java.cfg \
+    -providerName SunPKCS11-OpenSC \
+    -importkeystore \
+    -srckeystore certificate.p12 -srcstoretype PKCS12 -srcstorepass:file passin.txt \
+    -destkeystore NONE -deststoretype PKCS11
